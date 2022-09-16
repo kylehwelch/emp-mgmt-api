788a1a892274f1e0623240fc782575cc9c6d9296
kylehwelch <welch@udel.edu>
2022-09-16 16:18:29 -0400

    save button working

--- src/main/java/com/kyle/learn/employee/controller/EmployeeController.java
+++ src/main/java/com/kyle/learn/employee/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.kyle.learn.employee.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.kyle.learn.employee.model.Employee;
 import com.kyle.learn.employee.services.EmployeeService;
 
+@CrossOrigin(origins ="http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
     
-    @Autowired
     private EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
