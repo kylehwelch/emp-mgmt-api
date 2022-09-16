621441c279aa0c7c4c40ec5f0cbb15297d150378
kylehwelch <welch@udel.edu>
2022-09-16 12:57:43 -0400

    first commit

---
+++ src/test/java/com/kyle/learn/employee/EmpMgmtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyle.learn.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpMgmtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
